ec94df18f357b86f6fd4594867330e96ddfe353b
Gens81 <98492731+Gens81@users.noreply.github.com>
2023-11-03 22:05:23 +0100

    Update FFVI_compiler.bat

--- BNW-post-2.1-modifications-main/FFVI_compiler.bat
+++ BNW-post-2.1-modifications-main/FFVI_compiler.bat
@@ -28,20 +28,9 @@ echo Applying ips...
 %FLIPS% --apply "minimap.ips" %EDITED_ROM%
 %FLIPS% --apply "docileNPCs.ips" %EDITED_ROM%
 %FLIPS% --apply "newnarshe.ips" %EDITED_ROM%
-rem %FLIPS% --apply "msu-1.ips" %EDITED_ROM%
 
 echo Applying hacks...
 %ASAR% --pause-mode=on-error %PATCHES%\main.asm %EDITED_ROM%
 
 echo Creating patch...
 %FLIPS% --create --ips %VANILLA_ROM% %EDITED_ROM% "[n]BNW 2.2 B21.ips"
-rem %FLIPS% --create --ips %VANILLA_ROM% %EDITED_ROM% "[n]BNW 2.2 B21 (for real hardware).ips"
-
-echo Compressing GFX
-rem %FFVIDECOMP% -m c -s 0x02686C %EDITED_ROM% < %GFX%\modified\02686C_Title_Program.bin
-rem %FFVIDECOMP% -m c -s 0x18F000 %EDITED_ROM% < %GFX%\modified\18F000_Title_GFX.bin
-rem %FFVIDECOMP% -m c -s 0x19568f %EDITED_ROM% < %GFX%\modified\19568f_Ending_Cinematic_GFX_1.bin
-rem %FFVIDECOMP% -m c -s 0x199d4b %EDITED_ROM% < %GFX%\modified\199d4b_Ending_Cinematic_GFX_2.bin
-rem %FFVIDECOMP% -m c -s 0x19a4e5 %EDITED_ROM% < %GFX%\modified\19a4e5_Ending_Cinematic_GFX_3.bin
-rem %FFVIDECOMP% -m c -s 0x19a800 %EDITED_ROM% < %GFX%\modified\19a800_Map_Tile_Properties.bin
-rem %FFVIDECOMP% -m c -s 0x19cd10 %EDITED_ROM% < %GFX%\modified\19cd10_Map_Tile_Properties_Pointers.bin
