8196ba64c2eb2df89a2bfeb222e27e1ec464c6fe
Gens81 <98492731+Gens81@users.noreply.github.com>
2023-11-08 10:56:50 +0100

    Update misc.asm

--- BNW-post-2.1-modifications-main/ASM/misc.asm
+++ BNW-post-2.1-modifications-main/ASM/misc.asm
@@ -677,27 +677,25 @@ org $ED7D75
 ; edit jump table to skip over Window/Short check
 org $C3234E : dw $236F
 
-; Good Doggy
-; author: Bropedio
+;; Good Doggy
+;; author: Bropedio
+;;
+;; Skip showing damage on Interceptor when he blocks
 ;
-; Skip showing damage on Interceptor when he blocks
-
-!free = $C2FBB8 ; 10 bytes
-!warn = $C2FBC2
-
-org $C23451 : JSR ShouldSkipDog ; include Doggy in Zinger check
-
-org !free
-ShouldSkipDog:
-  TDC             ; A=0000
-  BIT $3A82       ; Doggy block in hibyte ($3A83) - N: NoDog, Z: On
-  BPL .exit       ; skip hit if Doggy block (no dmg numbers)
-  CPY $33F8       ; [displaced] Check if target is Zingered
-.exit
-  RTS             ; if Z flag is set, will not be targeted
-warnpc !warn
-
-hirom
+;!free = $C2FBB8 ; 10 bytes
+;!warn = $C2FBC2
+;
+;org $C23451 : JSR ShouldSkipDog ; include Doggy in Zinger check
+;
+;org !free
+;ShouldSkipDog:
+;  TDC             ; A=0000
+;  BIT $3A82       ; Doggy block in hibyte ($3A83) - N: NoDog, Z: On
+;  BPL .exit       ; skip hit if Doggy block (no dmg numbers)
+;  CPY $33F8       ; [displaced] Check if target is Zingered
+;.exit
+;  RTS             ; if Z flag is set, will not be targeted
+;warnpc !warn
 
 ; Prelude School
 ; author: Bropedio
