1cc5bfe1e8b5dd42a492af1ac1c69f3cc2843d7f
DESKTOP-68SDJIN\Utenye1 <dodinazareno@gmail.com>
2024-03-04 17:22:59 +0100

    Init Target and Power Hack

--- BNW-post-2.1-modifications-main/ASM/magic.asm
+++ BNW-post-2.1-modifications-main/ASM/magic.asm
@@ -150,13 +150,50 @@ PADBYTE $FF :	PAD $C327FE
 warnpc $C327FE
 
 org $C3020F
-	dw C327E2
+	dw C327F2
+
+; Queue pushing Y OAM fn	
+org $C32097
+	JSR C327E5			; Jump to routine that load sprite when entering skill submenu
 	
-org $C327FE
+org $C35975	
+	JSR C327E2
+
+org $C32d75
+	JMP C327EF
+	
+	
+org $C3a205
+C327E2:
+	JSR $0F11
+	BRA skip
+C327E5:
+	LDA $4B
+	ASL
+skip:
+	PHA
+	TDC
+	LDY #C36122			; $C3/6122
+	JSR $1173			; Queue OAM fn
+	TDC
+	PLA
+	RTS
+	
+C327EF:
+	JSR $3541
+	BRA skip
+
+PADBYTE $FF
+PAD $C3A222
+warnpc $C3A222
+
+org $C327E2	
 ; 1A: Sustain Magic menu
-C327E2:  LDA #$10        ; Description: On
-         TRB $45         ; Set menu flag
-         LDA #$01        ; List type: Magic
+
+C327F2:  LDA $DF
+		 LDA #$10        ; Description: On
+		 TRB $45         ; Set menu flag
+		 LDA #$01        ; List type: Magic
          STA $2A         ; Set redraw mode
          JSR $0EFD      ; Queue list upload
          LDA $021E       ; Frame counter
@@ -170,13 +207,75 @@ C327FB:  JSR $1F64      ; Handle L and R
          JSR $4B88      ; Handle D-Pad
          JSR $56E5      ; Load description
 
+; Holding Y
+Y_spec:	 lda $0D
+         bit #$40           	; holding Y?	
+		 BEQ C32822				; Branch if not
+		 LDA #$10				; Description off
+		 TSB $45				; Set
+		 STA $C0				; Set power and target flag
+		 jsl power_target_title	; Jump to write power and target
+		 RTS					; Go to original routine
+		
+PADBYTE $EA :	PAD $C32822
 warnpc $C32822		 
+org $C3281f
+C32822:
+;	jsr $6A41      ; Clear BG3 map B
+
 org $C32861
 C32861:
 
+org $C36122
+C36122:
+	JSL EDFD68		; Jump to routine that print y button
+	RTS
+warnpc $c36128
 
 ORG $C350DB
 	JSR C350AE
 	
 org $c3f7d1 
 	jsr C350A2
+
+org $C4b97b
+power_target_title2:
+	ldy #$0008
+	BRA power_target_title+3
+power_target_title:	
+	ldy #$0004
+	lda #$2c
+	sta $29
+	ldx #target_power_ptr
+	JSR C369BA
+	%FakeC3(0f4d)
+	RTL
+	
+target_power_ptr:
+	dw #target
+	dw #power
+	dw #target2
+	dw #power2
+	
+target:		dw $812f : db "Target",$00
+power:		dw $810f : db "Power",$00
+target2:	dw $814f : db "Target",$00
+power2:		dw $816f : db "Power",$00
+
+warnpc $c4b9ce
+
+org $C0DF00
+	db "Free",$00
+	db "Single",$00
+	db "One Foe",$00
+	db "Foe(s)",$00
+	db "Foe Group",$00
+	db "All Foes",$00
+	db "One Ally",$00
+	db "Allies",$00
+	db "Ally Group",$00
+	db "Party",$00
+	db "All",$00
+	
+warnpc $C0DF6B
+
--- BNW-post-2.1-modifications-main/ASM/pushing_Y_sprite.asm
+++ BNW-post-2.1-modifications-main/ASM/pushing_Y_sprite.asm
@@ -69,7 +69,7 @@ EDFD06:  dw EDFCFE       ; Top button sprite
          dw EDFCFE       ; Bogus... 
          db $FF          ; Loop
 
-; Update Y's sprite
+; Update Y's sprite shop
 EDFD12:  TAX             ; Index mode
          JMP (EDFD14,X)  ; Handle mode
 
@@ -109,5 +109,46 @@ EDFD2B:  LDA $47          ; Menu flags
          RTL              ; Exit
 .DELETE  CLC
 		 RTL
+		 
+; Update Y's sprite
+EDFD68:  TAX             ; Index mode
+         JMP (EDFD6C,X)  ; Handle mode
+		 
+; Jump table for the above
+EDFD6C:  dw EDFC71       ; Initialize button
+         dw EDFC90       ; Sustain button
+		 
+; Mode 0: Initialize Y
+EDFC71:  LDX $2D         ; Queue index
+         REP #$20        ; 16-bit A
+         LDA #EDFD06     ; Anim table ptr
+         STA $32C9,X     ; Set sprite's
+         SEP #$20        ; 8-bit A
+         LDA !bank       ; Bank: $ED
+         STA $35CA,X     ; Set ptr HB
+         %FakeC3(1206)   ; Set pose timer
+         INC $3649,X     ; Mode +1
+         LDA #$01        ; Pans with BG1
+         STA $364A,X     ; Set sprite flags
+		 
+; Mode 1: Sustain Y
+EDFC90:  LDA $26
+		 CMP #$0A
+		 BEQ .delete
+		 CMP #$4D
+		 BEQ .delete
+		 LDX $2D
+		 LDA #$E8         ; Cursor's X
+         STA $33CA,X      ; Set sprite's
+         LDA #$1f         ; Cursor's Y
+		 STA $344A,X      ; Set sprite's
+         %FakeC3(1221)    ; Define OAM
+		 SEC              ; Set to requeue
+         RTL              ; Exit
+.delete	 CLC
+		 RTL
+		 
+warnpc $edfe00		 
+
 org $ED5AC0
 incbin "../GFX/2D5AC0_Y_button_Sprite.bin"
