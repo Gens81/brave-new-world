1a28d3f9f81d42cbf9188a06cd22f36654557a11
Gens81 <98492731+Gens81@users.noreply.github.com>
2024-04-07 22:44:02 +0200

    Update reserved_freespace.asm

--- BNW-post-2.1-modifications-main/ASM/reserved_freespace.asm
+++ BNW-post-2.1-modifications-main/ASM/reserved_freespace.asm
@@ -1,22 +1,35 @@
 hirom
 
-; This assembly file "reserves" freespace for important alt patches by padding
-; them with an unconventional padding byte
+; This assembly file "reserves" freespace by padding them with an
+; unconventional padding byte
 
 padbyte $EE
 
-;idle time reducer
+; -----------------------------------------------------------------------------
+; Core
+; -----------------------------------------------------------------------------
 
+; summon descriptions
+org $CB52FC : pad $CB5790 ; 1172 bytes
+
+; animal descriptions (secondary screen)
+org $D8EE47 : pad $D8F000 ; 441 bytes
+
+; custom event scripting
+org $ED8BFC : pad $ED8E5B ; 607 bytes
+
+; -----------------------------------------------------------------------------
+; Alt Patches
+; -----------------------------------------------------------------------------
+
+; abridged_beta
 org $C0DAF9 : pad $C0DB01 ; 8 bytes
 
 ; notext_noswitch
-
 org $C0FF18 : pad $C0FF8E ; 118 bytes
 
 ; choreography
-
 org $C23AC5 : pad $C23AEE ; 41 bytes
 
 ; new_game_plus (Bropedio's randomizer)
-
 org $C3F612 : pad $C3F646 ; 52 bytes
