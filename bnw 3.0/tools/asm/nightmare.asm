888af7985fe6f37237e9cce07159b80d89daf938
Gens81 <98492731+Gens81@users.noreply.github.com>
2026-09-25 07:00:24 +0200

    Add files via upload

---
+++ bnw 3.0/tools/asm/nightmare.asm
@@ -0,0 +1,9 @@
+arch 65816
+hirom
+
+org $C457CD : db $94                    ; Enable "Show rider in vehicle" for Cyan NPC in the caves
+org $C457D0 : db $A0                    ; Enable magitek armour for Cyan NPC in the caves
+org $C457F3 : db $21                    ; Change soldier graphic to dancer graphic
+org $C457FC : db $21                    ; Change soldier graphic to dancer graphic
+org $C45805 : db $21                    ; Change soldier graphic to dancer graphic
+
